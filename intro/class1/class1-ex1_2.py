61a9c1c40aa4a1ad5ed90dc4eeed7ec5c7400803
Con Spathas <con.spathas@ardenta.com>
2015-04-24 16:34:22 -0700

    Commit

--- intro/class1/class1-ex1_2.py
+++
@@ -1,9 +0,0 @@
-v6 = 'FE80:0000:0000:0000:0101:A3EF:EE1E:1719'
-
-v6_list = v6.split(":")
-
-print v6_list
-
-v6_join = ":".join(v6_list)
-
-print v6_join
--- intro/class1/class1-ex3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import fileinput
-
-for line in fileinput.input():
-    line = line.strip()
-    print line.split(".")
