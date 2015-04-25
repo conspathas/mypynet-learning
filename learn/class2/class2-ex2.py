06120f26dc334153d2040b12ddb31f6266338386
Con Spathas <con.spathas@ardenta.com>
2015-04-25 06:55:49 -0700

    commit

--- learn/class2/class2-ex2.py
+++ learn/class2/class2-ex2.py
@@ -1,25 +1,16 @@
 user_input = raw_input('''
 
-Please enter an IP address using the following format options:-
-    172.16.1.0
-    172.16.1.
-    172.16.1
-
+Please enter an IP address. 
 Input: ''') 
 
 ip_addr_list = user_input.split(".")
-ip_addr_list = ip_addr_list[:3]
-ip_addr_list.append('0')
-
-ip_addr_string = ".".join(ip_addr_list)
-
-print "\n"
-print "The IP network is: %s" % ip_addr_string
 
 first_oct_bin = bin(int(ip_addr_list[0]))
-first_oct_hex = hex(int(ip_addr_list[0]))
+second_oct_bin = bin(int(ip_addr_list[1]))
+third_oct_bin = bin(int(ip_addr_list[2]))
+fourth_oct_bin = bin(int(ip_addr_list[3]))
 
 print "\n"
-print "%20s %20s %20s" % ("NETWORK_NUMBER", "FIRST_OCTET_BINARY", "FIRST_OCTET_HEX")
-print "%20s %20s %20s" % (ip_addr_string, first_oct_bin, first_oct_hex)
+print "%-20s %-20s %-20s %-20s" % ("first_octet", "second_octet", "third_octet", "fourth_octet")
+print "%-20s %-20s %-20s %-20s" % (first_oct_bin, second_oct_bin, third_oct_bin, fourth_oct_bin)
 
