770b05a7929f5a291d01da858c2862185c4dafbe
Con Spathas <con.spathas@ardenta.com>
2015-04-25 15:44:09 -0700

    commit

--- learn/class3/class3-ex1.py
+++ learn/class3/class3-ex1.py
@@ -1,26 +1,21 @@
-entry1 = "*  1.0.192.0/18   157.130.10.233        0 701 38040 9737 i"
-entry2 = "*  1.1.1.0/24      157.130.10.233         0 701 1299 15169 i"
-entry3 = "*  1.1.42.0/24     157.130.10.233        0 701 9505 17408 2.1465 i"
-entry4 = "*  1.0.192.0/19   157.130.10.233        0 701 6762 6762 6762 6762 38040 9737 i"
+#!/usr/bin/env python
 
-print "\n%-15s %-15s" % ("ip_prefix", "as_path")
+import sys
 
-split1 = entry1.split(" ")
-splice1_start = split1.index('701')
-splice1 = split1[splice1_start:-1]
-print "%-15s %-15s\n" % (split1[2], splice1)
+if len(sys.argv) == 2:
+    ip_addr = sys.argv.pop()
+    print "The IP address is: %s" % ip_addr
+else:
+    print "You made an error"
 
-split2 = entry2.split(" ")
-splice2_start = split2.index('701')
-splice2 = split2[splice2_start:-1]
-print "%-15s %-15s\n" % (split2[2], splice2)
+ip_addr_list = ip_addr(".")
 
-split3 = entry3.split(" ")
-splice3_start = split3.index('701')
-splice3 = split3[splice3_start:-1]
-print "%-15s %-15s\n" % (split3[2], splice3)
+first_oct_bin = bin(int(ip_addr_list[0]))
+second_oct_bin = bin(int(ip_addr_list[1]))
+third_oct_bin = bin(int(ip_addr_list[2]))
+fourth_oct_bin = bin(int(ip_addr_list[3]))
 
-split4 = entry4.split(" ")
-splice4_start = split4.index('701')
-splice4 = split4[splice4_start:-1]
-print "%-15s %-15s\n" % (split4[2], splice4)
+print "\n"
+print "%-20s %-20s %-20s %-20s" % ("first_octet", "second_octet", "third_octet", "fourth_octet")
+print "%-20s %-20s %-20s %-20s" % (first_oct_bin, second_oct_bin, third_oct_bin, fourth_oct_bin)
+
