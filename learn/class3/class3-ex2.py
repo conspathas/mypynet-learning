1c086d8adb2b1c084d8907e0dd8545a3f658601d
Con Spathas <con.spathas@ardenta.com>
2015-04-25 17:11:45 -0700

    commit

--- learn/class3/class3-ex2.py
+++ learn/class3/class3-ex2.py
@@ -5,27 +5,8 @@ entry4 = "*  1.0.192.0/19   157.130.10.233        0 701 6762 6762 6762 6762 3804
 
 print "\n%-15s %-15s" % ("ip_prefix", "as_path")
 
-
-
-
-
-
-split1 = entry1.split(" ")
-splice1_start = split1.index('701')
-splice1 = split1[splice1_start:-1]
-print "%-15s %-15s\n" % (split1[2], splice1)
-
-split2 = entry2.split(" ")
-splice2_start = split2.index('701')
-splice2 = split2[splice2_start:-1]
-print "%-15s %-15s\n" % (split2[2], splice2)
-
-split3 = entry3.split(" ")
-splice3_start = split3.index('701')
-splice3 = split3[splice3_start:-1]
-print "%-15s %-15s\n" % (split3[2], splice3)
-
-split4 = entry4.split(" ")
-splice4_start = split4.index('701')
-splice4 = split4[splice4_start:-1]
-print "%-15s %-15s\n" % (split4[2], splice4)
+for per_entry in (entry1, entry2, entry3, entry4):
+    split_entry = per_entry.split(" ")
+    splice_entry_start = split_entry.index('701')
+    splice_entry = split_entry[splice_entry_start:-1]
+    print "\n%-15s %-15s" % (split_entry[2], splice_entry)
